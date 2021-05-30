6bcfff9a64cdb9f1ecc1d155f7a983b6e0cb99c8
squareandroot <77972840+squareandroot@users.noreply.github.com>
2021-05-30 14:25:03 +0200

    Update and document toy_model.py

--- toy_model.py
+++ toy_model.py
@@ -3,7 +3,18 @@ import matplotlib.pyplot as plt
 from scipy import fft
 
 
-def f(u, x, j, Re):
+def f(u: np.array, x: np.array, j: int, Re: int) -> float:
+    """RHS of the discretised equation in exercise 13
+
+    Args:
+        u (np.array): Solution u at some time.
+        x (np.array): space discretisation.
+        j (int): index of current point in space.
+        Re (int): Reynolds number.
+
+    Returns:
+        float: RHS evaluated at current gridpoints.
+    """
     h = x[1] - x[0]
     u0 = np.roll(u, 1)[j]
     u1 = u[j]
@@ -13,24 +24,49 @@ def f(u, x, j, Re):
     return res
 
 
-def explicitEuler(f, Re, t=1e-3, n=100):
-    U = np.zeros((int(1/t), n))
+def explicitEuler(f, Re: int, t: float = 1e-3, n: int = 100) -> np.array:
+    """explicit Euler method
+
+    Args:
+        f (function): RHS of equation.
+        Re (int): Reynolds nubmer.
+        t (float, optional): time step size. Defaults to 1e-3.
+        n (int, optional): grid points for discretisation. Defaults to 100.
+
+    Returns:
+        np.array: solution u on a grid.
+    """
+    m = int(1/t) + 1
+    U = np.zeros((m, n))
     x = np.linspace(0, 1, n, endpoint=False)
-    for i in range(len(U) - 1):
-        for j in range(len(x)):
+    for i in range(m - 1):
+        for j in range(n):
             U[i + 1, j] = U[i, j] + t * \
                 f(U[i, :], x, j, Re)
     return U
 
 
-def plotting(Re_list):
+def plotting(Re_list: list) -> list:
+    """plot positive coefficients of the Fourier transformed
+    solution for the discretisation at t = 0.6 using different
+    values for Re.
+
+    Args:
+        Re_list (list): values of Re to solve pde for and plot the result.
+
+    Returns:
+        list: fourier transforms of solution for every Re
+    """
+    all_fft_U = list()
     for Re in Re_list:
-        U = explicitEuler(f, Re)[599]
+        U = explicitEuler(f, Re)[600]
         fft_U = fft.fft(U)[0:len(U)//2]
-        plt.loglog(np.abs(fft_U), label = "Re = " + str(Re))
+        plt.loglog(np.abs(fft_U), label="Re = " + str(Re))
+        all_fft_U.append(fft_U)
     plt.legend()
     plt.title("Fourier coefficients corresponding to positive frequencies")
     plt.savefig("toy_model.pdf", bbox_inches="tight", pad_inches=0.05)
+    return all_fft_U
 
 
-plotting([50, 200, 800])
+U = plotting([50, 200, 800])
